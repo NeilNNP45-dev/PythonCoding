0eaebc26920f9ce8f77515716225af45b61c41ce
NeilNNP45-dev <neilnnp45@gmail.com>
2026-05-20 00:01:21 +0530

    Delete Start.py
    Reuploaded with correct name later

--- Start.py
+++
@@ -1,4 +0,0 @@
-width = float(input("Enter the width of the rectangle"))
-length = float(input("Enter the length of the rectangle"))
-area =width*length
-print(f"The area of the rectangle is {area}")
